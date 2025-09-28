d7b85cb0dc18022da8d7befd0a73b0e4e51d46d1
Arnobio <arnobioaires2@outlook.com>
2025-09-28 01:43:35 -0300

    inclusão de testes

---
+++ Teste/Domain/Entidades/AdministradorTest.cs
@@ -0,0 +1,27 @@
+﻿namespace minimalApi.Domain.Entities;
+
+[TestClass]
+public sealed class AdministradorTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+
+        // Arrange
+        var administrador = new Administrador();
+    
+
+        // act
+        administrador.Id = 1;
+        administrador.Perfil = "Adm";
+        administrador.Email = "joao@gmail.com";
+        administrador.Senha = "teste123";
+
+        // assert
+        Assert.AreEqual(1, administrador.Id);
+        Assert.AreEqual("joao@gmail.com", administrador.Email);
+        Assert.AreEqual("Adm", administrador.Perfil);
+        Assert.AreEqual("teste123", administrador.Senha);
+    }
+}
+
